92b8c963c0c53268b3fde41f7430d40a54367804
calebjo <caleb.ri.jones@gmail.com>
2021-11-16 14:15:46 -0500

    Fix views for music_app

--- music_app/app/controllers/sessions_controller.rb
+++ music_app/app/controllers/sessions_controller.rb
@@ -9,14 +9,14 @@ class SessionsController < ApplicationController
     def create
         # create a session with user's params
         @user = User.find_by_credentials(
-            params[:user][:username]
+            params[:user][:username],
             params[:user][:password]
         )
 
         # if user is created successfully, log them in and redirect
         if @user.save
             login!(@user)
-            redirect_to :index # UsersController#show
+            redirect_to users_url(@user) # UsersController#show
         else # otherwise try again
             render :new
         end
--- music_app/config/routes.rb
+++ music_app/config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:create, :new, :show]
   resource :session, only: [:create, :new, :destroy]
+
+  root to: redirect('/session/new')
 end
